6a222c3dd1dd6002a9cdda370a72f90fd1a10e0c
btugut <berktugut@gmail.com>
2019-03-01 12:32:19 +0300

    Initial Commit

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -27,12 +27,13 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task <IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            // validate request
+
 
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
             
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exist");
+
             var userToCreate = new User
             {
                 Username = userForRegisterDto.Username
